242aa37f9a7efac62b4dba6f8d8185c9f4c30d5c
Alexandr Pimikov <apimikov@student.hive.fi>
2024-04-30 10:07:20 +0300

    casting

--- srcs/lin_alg/vec3.c
+++ srcs/lin_alg/vec3.c
@@ -2,46 +2,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef float t_num;  //norms???
 
-typedef struct s_vec3
+typedef enum e_obj_type
 {
-	float x;
-	float y;
-	float z;
-}	t_vec3;
+	SPHERE,
+	PLANE
+}	t_obj_type;
+
+typedef struct s_vec3d
+{
+	t_num	x;
+	t_num	y;
+	t_num	z;
+}	t_vec3d;
+
+typedef struct s_col
+{
+	int	r;
+	int	g;
+	int b;
+} t_col;
 
 typedef struct s_sphere
 {
-	float d;
-	float r[3];
-	float c[3];
-	t_vec3 rr;
+	int		type;
+	t_num	d;
+	t_vec3d	r;
+	t_col	c;
 }	t_sphere;
 
-float take_x(float r[3])
+typedef struct s_plane
 {
-	return (r[0]);
+	int		type;
+	t_vec3d	r;
+	t_vec3d	n;
+	t_col 	c;
+}	t_plane;
+
+
+typedef struct s_obj
+{
+	int	type;
+} t_obj;
+
+t_num	take_x_from_sphere(t_sphere *sphere)
+{
+	return (sphere->r.x);
+}
+
+t_num	take_x_from_plane(t_plane *plane)
+{
+	return (plane->r.x);
+}
+
+t_num	take_x(t_obj *obj)
+{
+	t_num ret;
+
+	if (obj->type == SPHERE)
+		ret = take_x_from_sphere((t_sphere*)obj);
+	else if (obj->type == PLANE)
+		ret =take_x_from_plane((t_plane*)obj);
+	return (ret);
 }
 
 int main()
 {
 	t_sphere s;
-	t_sphere *ps;
-	float r2[2];
+	t_plane p;
+	t_obj *o[2];
 
-	ps = malloc(sizeof(*ps));
-	ps->r[0] = 3;
-	ps->rr.x = 5;
-	printf("x=%f\n", ps->r[0]);
-	printf("x=%f\n", ps->rr.x);
-	printf("x=%f\n", take_x(ps->r));
-	printf("x=%f\n", take_x(r2));
+	s.r.x = 1;
+	s.type = SPHERE;
+	p.r.x = 2;
+	p.type = PLANE;
+	o[0] = (t_obj*)&s;
+	o[1] = (t_obj*)&p;
 
-	s.r[0] = 3;
-	s.rr.x = 5;
-	printf("x=%f\n", s.r[0]);
-	printf("x=%f\n", s.rr.x);
-
+	printf("sphere's x =%f\n", take_x(o[0]));
+	printf("plane's x =%f\n", take_x(o[1]));
 
 	return (1);
 }
